f0cfaa43de370ceb2ed906146a1f40796dd0e35c
fatkulumar <fatkulumar@gmail.com>
2019-04-15 18:38:04 +0700

    belajar laravel vue

---
+++ database/seeds/postTableSedder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Post;
+
+class postTableSedder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create('id_ID');
+               foreach (range(1, 100) as $loop) {
+            DB::table('posts')->insert([
+                'title'      => 'Welcome',
+                'description'       => str_slug('Welcome'),
+            ]);
+        }
+    }
+}
